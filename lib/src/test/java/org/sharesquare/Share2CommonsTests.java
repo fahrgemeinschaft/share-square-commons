d885cc6f9ad89e3e2a0b73991310167c3bc92558
zero <zero@sqrls.de>
2020-02-21 09:42:38 +0100

    empty test packages

---
+++ lib/src/test/java/org/sharesquare/Share2CommonsTests.java
@@ -0,0 +1,30 @@
+package org.sharesquare;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration
+@SpringBootApplication
+public class Share2CommonsTests {
+
+    Logger logger = LoggerFactory.getLogger(Share2CommonsTests.class);
+
+
+    @Test
+    public void contextLoads() {
+
+
+
+    }
+
+
+}
