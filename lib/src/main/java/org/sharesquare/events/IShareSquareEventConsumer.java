d76ae26f470491499624e28ada8366b3d8ff5ed4
zero <zero@sqrls.de>
2020-02-21 10:58:25 +0100

    moved unstable events to feature branch

--- lib/src/main/java/org/sharesquare/events/IShareSquareEventConsumer.java
+++
@@ -1,6 +0,0 @@
-package org.sharesquare.events;
-
-public interface IShareSquareEventConsumer<T> {
-
-    public void consume(ShareSquareEvent<T> event);
-}
--- lib/src/main/java/org/sharesquare/events/IShareSquareEventProducer.java
+++
@@ -1,7 +0,0 @@
-package org.sharesquare.events;
-
-public interface IShareSquareEventProducer<T> {
-
-
-    void produce(ShareSquareEvent<T> shareSquareEvent);
-}
