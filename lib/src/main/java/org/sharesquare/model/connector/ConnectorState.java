27631677e9668c84ac183935934cf66abf652d15
zero <zero@sqrls.de>
2020-03-23 19:04:19 +0100

    add connector Id to connectorState

--- lib/src/main/java/org/sharesquare/model/connector/ConnectorState.java
+++ lib/src/main/java/org/sharesquare/model/connector/ConnectorState.java
@@ -5,10 +5,16 @@ import lombok.EqualsAndHashCode;
 import org.sharesquare.AbstractShareSquareObject;
 
 import java.time.ZonedDateTime;
+import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class ConnectorState extends AbstractShareSquareObject {
+
+    /**
+     * Connector ID.
+     */
+    UUID connectorId;
 
     /**
      * Current State of Connector.
