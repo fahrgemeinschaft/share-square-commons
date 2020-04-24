29412e8c0b926fc953cb4db28295a80907855a71
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-04-24 21:07:57 +0200

    new offer fields start date and time and timezone

--- lib/src/main/java/org/sharesquare/model/Offer.java
+++ lib/src/main/java/org/sharesquare/model/Offer.java
@@ -5,6 +5,11 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.sharesquare.AbstractShareSquareObject;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,6 +18,14 @@ import java.util.UUID;
 public class Offer extends AbstractShareSquareObject {
 
     private String userId;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+    private LocalTime startTime;
+    
+    private ZoneId startTimezone=ZoneId.of("Europe/Berlin");
 
     private Location origin;
     private Location destination;
