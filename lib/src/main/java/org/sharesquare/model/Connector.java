2c94ff8adb5e34aedc100f78c887bd252f3fb164
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-07-17 13:24:25 +0200

    Swagger generates no valid URL for it's examples

--- lib/src/main/java/org/sharesquare/model/Connector.java
+++ lib/src/main/java/org/sharesquare/model/Connector.java
@@ -4,12 +4,17 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.sharesquare.AbstractShareSquareObject;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.net.URL;
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class Connector extends AbstractShareSquareObject {
 
+	@Schema(example = "http://pendlernetz.de/api/offers")
     URL offerUpdateWebhook;
+
+	@Schema(example = "http://pendlernetz.de/api/alivecheck")
     URL aliveCheckWebhook;
 
 }
--- lib/src/main/java/org/sharesquare/model/TargetSystem.java
+++ lib/src/main/java/org/sharesquare/model/TargetSystem.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.sharesquare.AbstractShareSquareObject;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.net.URL;
 
 @Data
@@ -12,6 +14,7 @@ public class TargetSystem extends AbstractShareSquareObject {
 
     String name;
     String description;
+    @Schema(example = "http://pendlernetz.de")
     URL vanityUrl;
     // ISO-639-1 conform
     String contentLanguage;
